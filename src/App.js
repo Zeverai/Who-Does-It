fcc3d11d42853b9184af5a54c952a4045bb0c8b3
Zeverai <zeverai.contact@gmail.com>
2022-02-11 11:41:32 -0500

    DELETED New Session Form and styles.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Navbar from "./components/Navbar";
-import NewSessionForm from "./pages/NewSessionForm";
 // ____________________________________________________________________________| STYLES
 import "./css/Global.css";
 // ____________________________________________________________________________| COMPONENT
@@ -14,7 +13,6 @@ const App = () => {
          <Navbar />
          <Routes>
             <Route exact path='/' element={<Landing />} />
-            <Route exact path='/newsessionform' element={<NewSessionForm />} />
          </Routes>
       </Router>
    );
